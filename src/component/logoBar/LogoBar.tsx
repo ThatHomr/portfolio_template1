040d4cdf91efa805d3214a1c098897c1e1029bcd
ThatHomr <34852381a@gmail.com>
2024-11-14 00:11:21 +0900

    fix(Samsung): svg Samsung component 추가

---
+++ src/component/logoBar/LogoBar.tsx
@@ -0,0 +1,54 @@
+import { ColorCode } from "@/style/color/Color";
+import { hexToRgba } from "@/utils/utils";
+import styled from "styled-components";
+import Google from "../common/svg/Google";
+import Nike from "../common/svg/Nike";
+
+interface ContainerProps {
+  $whiteColorRGBA: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  width: 1280px;
+  padding: 42px 24px;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  gap: 68px 86px;
+  flex-wrap: wrap;
+  background: ${({ $whiteColorRGBA }) => $whiteColorRGBA};
+  .imgGoogle {
+    width: 116.074px;
+    height: 38.209px;
+    flex-shrink: 0;
+  }
+  .imgNike {
+    width: 86.425px;
+    height: 30.741px;
+    flex-shrink: 0;
+  }
+`;
+
+const LogoBar: React.FC = ({}) => {
+  const whiteColorRGBA: string = hexToRgba(
+    ColorCode.White.hex,
+    ColorCode.White.opacity
+  );
+  const blackColorRGBA: string = hexToRgba(
+    ColorCode.Black.hex,
+    ColorCode.Black.opacity
+  );
+
+  return (
+    <Container $whiteColorRGBA={whiteColorRGBA}>
+      <div className="imgGoogle">
+        <Google color={blackColorRGBA} width={116.074} height={38.209} />
+      </div>
+      <div className="imgNike">
+        <Nike color={blackColorRGBA} width={86.425} height={30.741} />
+      </div>
+    </Container>
+  );
+};
+export default LogoBar;
