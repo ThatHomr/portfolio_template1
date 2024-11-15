14be62c10be7a5cd7306838f461456f79294ccb6
ThatHomr <34852381a@gmail.com>
2024-11-15 23:49:15 +0900

    fix(TestimonialCardBig): TestimonialCardBig component 작업중

---
+++ src/component/common/testimonialCard/TestimonialCardBig.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export interface TestimonialCardBigProps {}
+
+interface ContainerProps {}
+
+const Container = styled.div<ContainerProps>``;
+
+const TestimonialCardBig: React.FC<TestimonialCardBigProps> = ({}) => {
+  return <Container></Container>;
+};
+export default TestimonialCardBig;
